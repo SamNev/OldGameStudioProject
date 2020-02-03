57d3521ddeed5bca07cae4c7cedf21bc1964e856
SamNev <sam@nevillestuff.com>
2020-02-03 17:12:35 +0000

    Created Project (hopefully)

---
+++ Project/UnrealProject/TheProject/Source/TheProject/TheProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEPROJECT_API ATheProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
