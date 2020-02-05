8380a8d9a1701a8f6eb1bcbc89ebb61d938f147d
Paul Brown (s5111428) <s5111428@bournemouth.ac.uk>
2020-02-05 12:07:13 +0000

    Project Set up. Should work with Visual Studio 2019 now and included the player characters

---
+++ Project/UnrealProject/TheProject/Intermediate/Build/Win64/UE4Editor/Inc/TheProject/MyActor.gen.cpp
@@ -0,0 +1,99 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "TheProject/MyActor.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMyActor() {}
+// Cross Module References
+	THEPROJECT_API UClass* Z_Construct_UClass_AMyActor_NoRegister();
+	THEPROJECT_API UClass* Z_Construct_UClass_AMyActor();
+	ENGINE_API UClass* Z_Construct_UClass_AActor();
+	UPackage* Z_Construct_UPackage__Script_TheProject();
+// End Cross Module References
+	void AMyActor::StaticRegisterNativesAMyActor()
+	{
+	}
+	UClass* Z_Construct_UClass_AMyActor_NoRegister()
+	{
+		return AMyActor::StaticClass();
+	}
+	struct Z_Construct_UClass_AMyActor_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_myFloat_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FFloatPropertyParams NewProp_myFloat;
+		static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[];
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AMyActor_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AActor,
+		(UObject* (*)())Z_Construct_UPackage__Script_TheProject,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMyActor_Statics::Class_MetaDataParams[] = {
+		{ "IncludePath", "MyActor.h" },
+		{ "ModuleRelativePath", "MyActor.h" },
+	};
+#endif
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMyActor_Statics::NewProp_myFloat_MetaData[] = {
+		{ "Category", "MyActor" },
+		{ "ModuleRelativePath", "MyActor.h" },
+	};
+#endif
+	const UE4CodeGen_Private::FFloatPropertyParams Z_Construct_UClass_AMyActor_Statics::NewProp_myFloat = { "myFloat", nullptr, (EPropertyFlags)0x0010000000000004, UE4CodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(AMyActor, myFloat), METADATA_PARAMS(Z_Construct_UClass_AMyActor_Statics::NewProp_myFloat_MetaData, ARRAY_COUNT(Z_Construct_UClass_AMyActor_Statics::NewProp_myFloat_MetaData)) };
+	const UE4CodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_AMyActor_Statics::PropPointers[] = {
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AMyActor_Statics::NewProp_myFloat,
+	};
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AMyActor_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AMyActor>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AMyActor_Statics::ClassParams = {
+		&AMyActor::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		Z_Construct_UClass_AMyActor_Statics::PropPointers,
+		nullptr,
+		ARRAY_COUNT(DependentSingletons),
+		0,
+		ARRAY_COUNT(Z_Construct_UClass_AMyActor_Statics::PropPointers),
+		0,
+		0x009000A0u,
+		METADATA_PARAMS(Z_Construct_UClass_AMyActor_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_AMyActor_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AMyActor()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AMyActor_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AMyActor, 1763322863);
+	template<> THEPROJECT_API UClass* StaticClass<AMyActor>()
+	{
+		return AMyActor::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AMyActor(Z_Construct_UClass_AMyActor, &AMyActor::StaticClass, TEXT("/Script/TheProject"), TEXT("AMyActor"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AMyActor);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
